e700a075bf75a0093bd5d59963026c72dad52f0b
Feyzullah Durası <128799284+feyzullahdurasi@users.noreply.github.com>
2024-05-11 15:05:07 +0300

    Update MapsViewController

--- FavoriKonum/MapsViewController.swift
+++ FavoriKonum/MapsViewController.swift
@@ -20,6 +20,14 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
     var secilenLatitude = Double()
     var secilenLongitude = Double()
     
+    var secilenIsim = ""
+    var secilenId : UUID?
+    
+    var annotationTitle = ""
+    var annotationSubtitle = ""
+    var annotationLatitude = Double()
+    var annotationLongitude = Double()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -29,10 +37,124 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
         
-        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(konumSec(gestureRecognizer: )))
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(konumSec(gestureRecognizer:)))
         gestureRecognizer.minimumPressDuration = 1
         mapView.addGestureRecognizer(gestureRecognizer)
         
+        if secilenIsim != "" {
+            if let uuidString = secilenId?.uuidString {
+                
+                if let uuidString = secilenId?.uuidString {
+                    print(uuidString)
+                }
+                
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appDelegate.persistentContainer.viewContext
+                
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Yer")
+                fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)
+                fetchRequest.returnsObjectsAsFaults = false
+                
+                do {
+                    let sonuclar = try context.fetch(fetchRequest)
+                    
+                    if sonuclar.count > 0 {
+                        
+                        for sonuc in sonuclar as! [NSManagedObject] {
+                            
+                            if let isim = sonuc.value(forKey: "isim") as? String {
+                                annotationTitle = isim
+                                
+                                if let not = sonuc.value(forKey: "not") as? String {
+                                    annotationSubtitle = not
+                                    
+                                    if let latitude = sonuc.value(forKey: "latitude") as? Double {
+                                        annotationLatitude = latitude
+                                        
+                                        if let longitude = sonuc.value(forKey: "longitude") as? Double {
+                                            annotationLongitude = longitude
+                                            
+                                            let annotation = MKPointAnnotation()
+                                            annotation.title = annotationTitle
+                                            annotation.subtitle = annotationSubtitle
+                                            let coordinate = CLLocationCoordinate2D(latitude: annotationLatitude, longitude: annotationLongitude)
+                                            annotation.coordinate = coordinate
+                                            
+                                            mapView.addAnnotation(annotation)
+                                            isimTextField.text = annotationTitle
+                                            notTextField.text = annotationSubtitle
+                                            
+                                            locationManager.stopUpdatingLocation()
+                                            
+                                            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                            
+                                            let region = MKCoordinateRegion(center: coordinate, span: span)
+                                            mapView.setRegion(region, animated: true)
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                            
+
+                        }
+                    }
+                } catch {
+                    print("hata")
+                }
+                
+            }
+        } else {
+            // yeni veri ekleye geldi
+        }
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: any MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let reUseId = "benimAnnotation"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reUseId)
+        
+        if pinView == nil {
+            
+            pinView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: reUseId)
+            pinView?.canShowCallout = true
+            pinView?.tintColor = .red
+            
+            let button = UIButton(type: .detailDisclosure)
+            pinView?.rightCalloutAccessoryView = button
+            
+        } else {
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if secilenIsim != "" {
+            let requestLocation = CLLocation(latitude: annotationLatitude, longitude: annotationLongitude)
+            
+            CLGeocoder().reverseGeocodeLocation(requestLocation) { (placemarkDizisi, hata) in
+                
+                if let placemarks = placemarkDizisi {
+                    if placemarks.count > 0 {
+                        let yeniPlacemark = MKPlacemark(placemark: placemarks[0])
+                        let item = MKMapItem(placemark: yeniPlacemark)
+                        item.name = self.annotationTitle
+                        
+                        let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+                        
+                        item.openInMaps(launchOptions: launchOptions)
+                        
+                    }
+                }
+                
+            }
+            
+        }
     }
     
     @objc func konumSec(gestureRecognizer : UILongPressGestureRecognizer) {
@@ -54,12 +176,16 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        //print(locations[0].coordinate.latitude)
-        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        print(locations[0].coordinate.latitude)
+        if secilenIsim == "" {
+            
+            let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+            
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            let region = MKCoordinateRegion(center: location, span: span)
+            mapView.setRegion(region, animated: true)
+        }
         
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        let region = MKCoordinateRegion(center: location, span: span)
-        mapView.setRegion(region, animated: true)
         
     }
     @IBAction func kaydetTıklandı(_ sender: Any) {
@@ -80,6 +206,9 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
         } catch {
             print("hata")
         }
+        
+        NotificationCenter.default.post(name: NSNotification.Name("yeniYerOldu"), object: nil)
+        navigationController?.popViewController(animated: true)
         
     }
     
